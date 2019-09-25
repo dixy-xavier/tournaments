be396fceb31dd69a0947a1540e4b5219de432806
DIXY <dixy@Dixys-MacBook-Pro.local>
2019-09-25 16:44:37 +0530

    project setup and routing

---
+++ src/components/Home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => (
+  <div>
+    Hello World
+  </div>
+);
+
+export default Home;
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HWP = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: path.join(__dirname, '/src/index.js'),
+  output: {
+    filename: 'build.js',
+    path: path.join(__dirname, '/dist'),
+  },
+  watch: true,
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+    }, {
+      test: /\.css$/,
+      use: [{
+        loader: 'style-loader'
+      }, {
+        loader: 'css-loader',
+        options: {
+          sourceMap: true,
+          modules: {
+            localIdentName: '[name]__[local]___[hash:base64:5]'
+          }
+        }
+      }]
+    }],
+  },
+  plugins: [
+    new HWP(
+      {
+        template: path.join(__dirname, '/index.html'),
+        hash: true
+      },
+    ),
+  ],
+};
