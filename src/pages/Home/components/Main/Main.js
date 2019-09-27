6843d1228be81bb4c8fbe4cc9fc3a6101d90c72a
dixy-xavier <dixyxavier@gmail.com>
2019-09-27 21:37:01 +0530

    adds json server

--- src/pages/Home/components/Main/Main.js
+++ src/pages/Home/components/Main/Main.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import TournamentsContainer from './components/TournamentsContainer/TournamentsContainer';
-import { tournaments as list } from './Main.constants';
+import { GET_TOURNAMENTS } from './Main.constants';
 import styles from './Main.css';
-
-const tournaments = list.map(tournament => ({
-  ...tournament,
-  date_start: new Date(tournament.date_start.split('Z')[0]),
-  date_end: new Date(tournament.date_end.split('Z')[0]),
-}));
+import { request } from './utils/utils';
 
 const Main = ({ searchText, startDate, endDate }) => {
+  const [tournaments, setTournaments] = useState([]);
+  useEffect(() => {
+    request(GET_TOURNAMENTS).then(response => {
+      if (response.length) {
+        const list = response.map(tournament => ({
+          ...tournament,
+          date_start: new Date(tournament.date_start.split('Z')[0]),
+          date_end: new Date(tournament.date_end.split('Z')[0]),
+        }));
+        setTournaments(list);
+      }
+    });
+  }, []);
   const filteredList = tournaments.filter(tournament => (
     tournament.series.name.toLowerCase().includes(searchText.toLowerCase()) &&
     tournament.date_start >= startDate &&
